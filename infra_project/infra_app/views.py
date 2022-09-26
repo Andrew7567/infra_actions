7f2ac7f1ac29666478245a407c04309f65ec655f
Andrew7567 <andrei.petrukhin@yandex.ru>
2022-09-26 21:06:35 +0300

    new commit

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from rest_framework import status
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!', status=status.HTTP_200_OK)
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    return HttpResponse('А это вторая страница!', status=status.HTTP_200_OK)
